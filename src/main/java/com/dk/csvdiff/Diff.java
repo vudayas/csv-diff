57c078f8e33805f0c55fc5f64ce32b74d3a05f7d
Darren Kennedy <darrenk1972@gmail.com>
2017-08-11 14:33:16 -0600

    Initial commit

---
+++ src/main/java/com/dk/csvdiff/Diff.java
@@ -0,0 +1,17 @@
+package com.dk.csvdiff;
+
+/**
+ * CSV Diff class
+ * Contains entry point
+ * and logic to diff two CSV data structures
+ * creating a new CSV with the differences.
+ * 
+ * @author Darren Kennedy
+ */
+public class Diff 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
